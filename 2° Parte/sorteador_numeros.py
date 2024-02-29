f05998dc0417786884e103aac91a2d76b9040ec5
devdinei <devdinei@gmail.com>
2024-02-29 00:29:13 -0300

    sorteador

---
+++ 2° Parte/sorteador_numeros.py
@@ -0,0 +1,7 @@
+print('O sorteio sera de 1 a 10 sendo Joao 1 / Pedro 2 / Jose 3 / dinei 4 / Thiago 5 / Geovane 6 / Fernanda 7 / Vini 8 / lipe 9 / eliana 10')
+
+import random
+
+num = random . randint(1, 10)
+
+print (' O numero escolhido é {0}'.format(num))
